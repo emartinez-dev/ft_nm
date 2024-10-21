60e93b2f766fde1e5f8db8a0f520497b1ac849d8
Enrique Martínez <enri.fc.98@gmail.com>
2024-10-21 23:00:21 +0200

    Added the missing types for the ft_nm binary, needs testing

--- src/output.c
+++ src/output.c
@@ -6,7 +6,7 @@
 /*   By: franmart <franmart@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 19:06:28 by franmart          #+#    #+#             */
-/*   Updated: 2024/10/19 13:56:02 by franmart         ###   ########.fr       */
+/*   Updated: 2024/10/21 22:59:57 by franmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ char    get_type_elf64(t_ELF64_symbol *sym, t_ELF64_section_header *section_h,
 {
 	char	class = '?';
 	char	*section_name = &shstrtab[section_h[sym->st_shndx].sh_name];
+	t_ELF64_section_header	*symbol_section = &section_h[sym->st_shndx];
 
 	if (section_h[sym->st_shndx].sh_type == SHN_UNDEF)
 	{
@@ -44,41 +45,25 @@ char    get_type_elf64(t_ELF64_symbol *sym, t_ELF64_section_header *section_h,
 		class = 'C';
 	else
 	{
-		if (ft_strncmp(section_name, ".text", 5) == 0)
+		if (ft_strncmp(section_name, ".text", 5) == 0 || ST_TYPE(sym->st_info) == STT_FUNC)
 		{
 			class = 'T';
 			class = (ST_BIND(sym->st_info) == STB_GLOBAL) ? 'T' : 't';
 		}
 		else if (ft_strncmp(section_name, ".bss", 4) == 0)
 			class = (ST_BIND(sym->st_info) == STB_GLOBAL) ? 'B' : 'b';
-		else if (ft_strncmp(section_name, ".rodata", 7) == 0)
-			class = 'R';
-		else if (ft_strncmp(section_name, ".data", 5) == 0)
-			class = 'D';
+		else if (ft_strncmp(section_name, ".dynamic", 8) == 0)
+			class = 'd';
+		else if (ft_strncmp(section_name, ".data", 5) == 0 || symbol_section->sh_flags & SHF_WRITE)
+		{
+			if (ST_BIND(sym->st_info) == STB_WEAK)
+				class = 'W';
+			else
+				class = (ST_BIND(sym->st_info) == STB_GLOBAL) ? 'D' : 'd';
+		}
+		else if (symbol_section->sh_flags != SHF_WRITE)
+			class = (ST_BIND(sym->st_info) == STB_GLOBAL) ? 'R' : 'r';
 	}
-	/* TODO: Implement these types
-	./ft_nm ft_nm > a ; LC_COLLATE=C nm ft_nm > b; diff a b
-	1,2c1,2
-	< 0000000000004dd8 ? _DYNAMIC
-	< 0000000000004fe8 ? _GLOBAL_OFFSET_TABLE_
-	---
-	> 0000000000004dd8 d _DYNAMIC
-	> 0000000000004fe8 d _GLOBAL_OFFSET_TABLE_
-	6c6
-	< 0000000000003128 ? __GNU_EH_FRAME_HDR
-	---
-	> 0000000000003128 r __GNU_EH_FRAME_HDR
-	18,19c18,19
-	< 0000000000002940 ? _fini
-	< 0000000000001000 ? _init
-	---
-	> 0000000000002940 T _fini
-	> 0000000000001000 T _init
-	24c24
-	< 0000000000005058 D data_start
-	---
-	> 0000000000005058 W data_start
-	*/
 	return (class);
 }
 
