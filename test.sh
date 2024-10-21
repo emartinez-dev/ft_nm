9aef639e38d5e683db9ecfd462382bb80b6b6bb9
Enrique Martínez <enri.fc.98@gmail.com>
2024-10-21 23:23:57 +0200

    Update script to accept multiple arguments

--- test.sh
+++ test.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
-# Check if an argument was provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <file>"
+# Check if at least an argument was provided
+if [ $# -eq 0 ]; then
+  echo "Usage: $0 <file1> <file2> ..."
   exit 1
 fi
 
-filename=$1
+for file in "$@"; do
+  # Check if the file exists
+  if [ -f "$file" ]; then
+	./ft_nm $file > a ; LC_COLLATE=C nm $file > b
 
-./ft_nm $filename > a ; LC_COLLATE=C nm $filename > b
+	# Save the diff output to a variable
+	diff_output=$(diff a b)
 
-# Save the diff output to a variable
-diff_output=$(diff a b)
+	# Check if the diff_output is empty
+	if [ -z "$diff_output" ]; then
+		echo "For '$file' the outputs are identical."
+	else
+		echo "For '$file', the outputs are different. Here's the diff:"
+		echo "$diff_output"
+	fi
 
-# Check if the diff_output is empty
-if [ -z "$diff_output" ]; then
-  echo "The outputs are identical."
-else
-  echo "The outputs are different. Here's the diff:"
-  echo "$diff_output"
-fi
-
-rm a b
+	rm a b
+	fi
+done
